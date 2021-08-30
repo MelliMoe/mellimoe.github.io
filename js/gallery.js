36e438d003f0d523ceadea45b98382896b3b7983
Sailunsi <sailuns@amazon.com>
2021-08-30 15:27:54 -0700

    test

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,4 @@
-$(document).ready(function(){$("[data-fancybox]").fancybox({selector:"a.image-thumb:visible",loop:!0,hash:!0,buttons:["thumbs","close"],transitionEffect:"slide",preventCaptionOverlap:!0})});
+$(function() {
+        $("#portfoliolist").mixItUp({selectors:{target:".portfolio",filter:".filter"}});
+    });
+    $(document).ready(function(){$("[data-fancybox]").fancybox({selector:"a.image-thumb:visible",loop:!0,hash:!0,buttons:["thumbs","close"],transitionEffect:"slide",preventCaptionOverlap:!0})});
