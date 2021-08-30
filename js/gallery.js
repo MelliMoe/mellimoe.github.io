360752447d794d4bb7ee5d3914c1254907a720b3
Sailunsi <sailuns@amazon.com>
2021-08-30 15:41:17 -0700

    test

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,1 @@
-$(function() {
-        $("#portfoliolist").mixItUp({selectors:{target:".portfolio",filter:".filter"}});
-    });
-    $(document).ready(function(){$("[data-fancybox]").fancybox({selector:"a.image-thumb:visible",loop:!0,hash:!0,buttons:["thumbs","close"],transitionEffect:"slide",preventCaptionOverlap:!0})});
+$(document).ready(function(){$("#portfoliolist").mixItUp({animation:{enable:false},selectors:{target:".portfolio",filter:".filter"}},callbacks:{onMixLoad:function(){$(this).mixItUp('setOptions',{animation:{enable:true}})}}),$("[data-fancybox]").fancybox({selector:"a.image-thumb:visible",loop:!0,hash:!0,buttons:["thumbs","close"],transitionEffect:"slide",preventCaptionOverlap:!0})});
